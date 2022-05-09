2d85a97a63c989f9c61192d4ddd55b8a1f24d289
Kasra <kasrahajian2002@gmail.com>
2022-05-09 17:44:42 +0430

    added DarkMode in preferences

--- app/src/main/java/edu/sharif/ce/android_weather_app/ui/preferences/PreferencesFragment.java
+++ app/src/main/java/edu/sharif/ce/android_weather_app/ui/preferences/PreferencesFragment.java
@@ -4,17 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.CompoundButton;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 import edu.sharif.ce.android_weather_app.databinding.FragmentPreferencesBinding;
 
 public class PreferencesFragment extends Fragment {
 
     private FragmentPreferencesBinding binding;
+    private SwitchMaterial darkModeSwitch;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,8 +28,18 @@ public class PreferencesFragment extends Fragment {
         binding = FragmentPreferencesBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textPreferences;
-        preferencesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        darkModeSwitch = binding.darkModeSwitch;
+        darkModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (darkModeSwitch.isChecked()) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
+
         return root;
     }
 
