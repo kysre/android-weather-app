005ddaea72f02e1bc41ad6f938baaa02c2779a98
Kasra <kasrahajian2002@gmail.com>
2022-05-09 14:26:57 +0430

    added preferences navigation

---
+++ app/src/main/java/edu/sharif/ce/android_weather_app/ui/preferences/PreferencesFragment.java
@@ -0,0 +1,37 @@
+package edu.sharif.ce.android_weather_app.ui.preferences;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import edu.sharif.ce.android_weather_app.databinding.FragmentPreferencesBinding;
+
+public class PreferencesFragment extends Fragment {
+
+    private FragmentPreferencesBinding binding;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        PreferencesViewModel preferencesViewModel =
+                new ViewModelProvider(this).get(PreferencesViewModel.class);
+
+        binding = FragmentPreferencesBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        final TextView textView = binding.textPreferences;
+        preferencesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        return root;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
